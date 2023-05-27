34f2fe99bbe506a25f11b7239f5c9842285125b9
Chandra Prakash Kumar <chandraprakash.bhr@gmail.com>
2023-05-27 23:50:54 +0530

    Added fetch all transactions api

--- finzo/src/main/java/com/example/finzo/Repository/TransactionRepo.java
+++ finzo/src/main/java/com/example/finzo/Repository/TransactionRepo.java
@@ -4,6 +4,9 @@ import com.example.finzo.entity.TransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepo extends JpaRepository<TransactionEntity, String> {
+    List<TransactionEntity> findByReceiverAccountId(String receiverAccountId);
 }
---
+++ finzo/src/main/java/com/example/finzo/serviceImpl/BalanceImpl.java
@@ -0,0 +1,47 @@
+package com.example.finzo.serviceImpl;
+
+import com.example.finzo.Exception.ResourceNotFoundException;
+import com.example.finzo.Repository.TransactionRepo;
+import com.example.finzo.Repository.UserAccountRepo;
+import com.example.finzo.entity.TransactionEntity;
+import com.example.finzo.entity.UserAccountEntity;
+import com.example.finzo.service.BalanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BalanceImpl implements BalanceService {
+    @Autowired
+    UserAccountRepo userAccountRepo;
+    @Autowired
+    TransactionRepo transactionRepo;
+
+    @Override
+    public Integer fetchBalanceByAccountNumber(String accountNumber) {
+        UserAccountEntity userAccountEntity = userAccountRepo.findById(accountNumber)
+                .orElseThrow(()-> new ResourceNotFoundException("User not found with ID :"+accountNumber));
+        return userAccountEntity.getBalance();
+    }
+
+    @Override
+    public Integer fetchBalanceByAadharNumber(String aadharNumber) {
+        Integer currentBalance = 0;
+        try{
+            UserAccountEntity userAccountEntity = userAccountRepo.findByAadharNumber(aadharNumber);
+            currentBalance = userAccountEntity.getBalance();
+        } catch (Exception ignored){
+            throw new ResourceNotFoundException("User not found with provided aadhar number "+ aadharNumber);
+        }
+
+        return currentBalance;
+    }
+
+    @Override
+    public List<TransactionEntity> fetchAllTransactions(String accountNumber) {
+        UserAccountEntity userAccountEntity = userAccountRepo.findById(accountNumber)
+                .orElseThrow(()-> new ResourceNotFoundException("User not found with ID :"+accountNumber));
+        return transactionRepo.findByReceiverAccountId(accountNumber);
+    }
+}
