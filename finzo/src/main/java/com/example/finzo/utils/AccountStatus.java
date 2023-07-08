ab943960c865a2372628d09c47d1a726e26e5ed7
Chandra Prakash Kumar <chandraprakash.bhr@gmail.com>
2023-07-08 16:28:56 +0530

    project structure change

--- finzo/src/main/java/com/example/finzo/utils/AccountStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.finzo.utils;
-
-public enum AccountStatus {
-    ACTIVE,
-    IN_ACTIVE,
-}
--- finzo/src/main/java/com/example/finzo/utils/TransactionType.java
+++
@@ -1,6 +0,0 @@
-package com.example.finzo.utils;
-
-public enum TransactionType {
-    DEPOSIT,
-    WITHDRAW
-}
